3ae6af4124bf8cdc3bee2c8aafdacee104b98b78
MahmoudSaeed85 <odasaeed71@gmail.com>
2026-07-13 00:39:46 +0400

    Add files via upload

--- Sad.java
+++ Sad.java
@@ -1,109 +1,105 @@
 package Tests;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Sad extends BaseTest {
 
     @Test
-    public void usernameWithSpacesAroundTest() {
+    public void invalidLoginTest() {
 
-        driver.findElement(By.id("user-name")).sendKeys("   visual_user   ");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("user-name"))
+                .sendKeys("performance_glitch_user");
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+        driver.findElement(By.id("password"))
+                .sendKeys("invalid_password");
+
+        driver.findElement(By.id("login-button"))
+                .click();
+
+        String actualMessage = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+
+        String expectedMessage =
+                "Epic sadface: Username and password do not match any user in this service";
+
+        Assert.assertEquals(actualMessage, expectedMessage);
     }
     @Test
-    public void emptyPasswordWithValidUserTest() {
+    public void invalidPasswordTest() {
 
-        driver.findElement(By.id("user-name")).sendKeys("visual_user");
+        driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+        driver.findElement(By.id("password")).sendKeys("wrong_password");
         driver.findElement(By.id("login-button")).click();
 
         String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        String expected = "Epic sadface: Username and password do not match any user in this service";
 
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void emptyUsernameTest() {
+
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        String expected = "Epic sadface: Username is required";
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void emptyPasswordTest() {
+
+        driver.findElement(By.id("user-name")).sendKeys("performance_glitch_user");
+        driver.findElement(By.id("login-button")).click();
+
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        String expected = "Epic sadface: Password is required";
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void emptyCredentialsTest() {
+
+        driver.findElement(By.id("login-button")).click();
+
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        String expected = "Epic sadface: Username is required";
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void usernameWithSpacesTest() {
+
+        driver.findElement(By.id("user-name")).sendKeys("   ");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
         Assert.assertEquals(actual,
-                "Epic sadface: Password is required");
+                "Epic sadface: Username is required");
     }
     @Test
-    public void usernameWithTrailingSpacesTest() {
+    public void sqlInjectionTest() {
 
-        driver.findElement(By.id("user-name")).sendKeys("visual_user    ");
+        driver.findElement(By.id("user-name")).sendKeys("' OR '1'='1");
+        driver.findElement(By.id("password")).sendKeys("' OR '1'='1");
+        driver.findElement(By.id("login-button")).click();
+
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        Assert.assertEquals(actual,
+                "Epic sadface: Username and password do not match any user in this service");
+    }
+    @Test
+    public void lockedOutUserTest() {
+
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
 
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+        String actual = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        Assert.assertEquals(actual,
+                "Epic sadface: Sorry, this user has been locked out.");
     }
-    @Test
-    public void usernameWithLeadingSpacesTest() {
-
-        driver.findElement(By.id("user-name")).sendKeys("    visual_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
-    }
-
-
-    @Test
-    public void verifyBackForwardSession() {
-
-        driver.findElement(By.id("user-name")).sendKeys("visual_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        driver.navigate().back();
-        driver.navigate().forward();
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
-    }
-    @Test
-    public void verifyWhitespaceTrimming() {
-
-        driver.findElement(By.id("user-name")).sendKeys("   visual_user   ");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
-    }
-    @Test
-    public void verifyCartIconAlignment() {
-
-        driver.findElement(By.id("user-name")).sendKeys("visual_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        WebElement cart = driver.findElement(By.className("shopping_cart_link"));
-
-        Assert.assertTrue(cart.isDisplayed());
-    }
-    @Test
-    public void verifyBurgerMenuAlignment() {
-
-        driver.findElement(By.id("user-name")).sendKeys("visual_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        WebElement menu = driver.findElement(By.id("react-burger-menu-btn"));
-
-        Assert.assertTrue(menu.isDisplayed());
-    }
-    @Test
-    public void verifyPriceConsistencyDetails() {
-
-        driver.findElement(By.id("user-name")).sendKeys("visual_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-
-        String inventoryPrice = driver.findElement(By.className("inventory_item_price")).getText();
-
-        driver.findElement(By.className("inventory_item_name")).click();
-
-        String detailsPrice = driver.findElement(By.className("inventory_details_price")).getText();
-
-        Assert.assertEquals(detailsPrice, inventoryPrice);
-    }
-    }
-
+}
